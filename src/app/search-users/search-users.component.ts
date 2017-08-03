a5189823e68f10db18b4ec959a9de58ecd9d1963
VitaliyBogdanuk <v.bogdanuk@gmail.com>
2017-08-03 11:18:39 +0300

    add aplication - search github users

--- src/app/search-users/search-users.component.ts
+++ src/app/search-users/search-users.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SearchUsersService } from '../search-users.service';
 
 @Component({
   selector: 'app-search-users',
@@ -6,10 +7,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search-users.component.css']
 })
 export class SearchUsersComponent implements OnInit {
+    place: string;
+    language: string;
 
-  constructor() { }
+    results: any[] = []; // This will hold the data coming from the service
+    selected: boolean = false; // Flag to check if a user is clicked or not
+    selectedUser: any; // presently Selected user details
+    error_text: string = ""; // So called error reporing text to the end user
 
-  ngOnInit() {
-  }
+    constructor(private searchService: SearchUsersService) {}
+    ngOnInit() {}
 
+    search(place: string, language: string) {
+        this.selected = false;
+        this.error_text = "";
+        if (place || language) {
+            this.place = place;
+            this.language = language;
+            this.searchService.getUsersByPlaceAndLanguage(place, language).subscribe(
+                users => {
+                    this.results = users;
+                },
+                error => {
+                    this.results = [];
+                    this.error_text = "Sorry! No Users found. Try again";
+                    console.error(error);
+                }
+            )
+        }
+    }
+
+    getDetails(username: string) {
+        this.searchService.getDetailsByUserName(username).subscribe(
+            userDatils => {
+                this.selectedUser = userDatils;
+                this.selected = true;
+            },
+            error => {
+                this.selected = false;
+                console.error(error);
+            }
+        )
+    }
 }
